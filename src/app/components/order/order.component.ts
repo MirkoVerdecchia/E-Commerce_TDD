31d0eb63159916077ea032cfddbf49f4af3e9232
Mirko <mirko.verdecchia@studenti.unicam.it>
2022-02-22 17:19:17 +0100

    Order component

---
+++ src/app/components/order/order.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import { IOrder } from 'src/app/interface/order';
+import { IProduct } from 'src/app/interface/product';
+import { CartService } from 'src/app/service/cart.service';
+
+@Component({
+  selector: 'app-order',
+  templateUrl: './order.component.html',
+  styleUrls: ['./order.component.css']
+})
+export class OrderComponent implements OnInit {
+
+  private cart: IProduct[] = [];
+
+
+  constructor(private cartService: CartService) { }
+
+  ngOnInit(): void {
+  }
+
+  
+  getCart() {
+    
+  }
+
+  removeFromCart(product: IProduct) {
+
+  }
+
+
+  makeOrder(phone: string, city: string, address: string) {
+
+  }
+
+
+  sendOrder(order: IOrder) {
+
+  }
+
+
+  checkAddressData(phone: string, city: string, via: string) {
+
+  }
+
+
+  emptyCart() {
+
+  }
+
+
+  getTotal() {
+
+  }
+
+
+  //restituisce la quantità di un prodotto nel carrello
+  numberProduct(p : IProduct) {
+
+  }
+
+
+}
