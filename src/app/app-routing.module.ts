a960fc8b5cf08c7534b228b3b6cc890adebcef8f
Mirko <mirko.verdecchia@studenti.unicam.it>
2022-02-22 17:17:43 +0100

    Frist settage - npm

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { OrderComponent } from './components/order/order.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: '', redirectTo: '/home', pathMatch: 'full'},
+{path: 'home', component: HomeComponent},
+{path: 'product-list', component: ProductListComponent},
+{path: 'order', component: OrderComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OrderComponent } from './components/order/order.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrderComponent,
+    HomeComponent,
+    NavbarComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule,
+    MatToolbarModule,
   ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
