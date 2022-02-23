db5e4a1d8cf3eceb58eefa8122d4c6ccc71c405a
Mirko <mirko.verdecchia@studenti.unicam.it>
2022-02-23 18:31:12 +0100

    test for order UI element

--- src/app/components/order/order.component.spec.ts
+++ src/app/components/order/order.component.spec.ts
@@ -7,36 +7,93 @@ import { OrderComponent } from './order.component';
 describe('OrderComponent', () => {
   let component: OrderComponent;
   let fixture: ComponentFixture<OrderComponent>;
-  let cartService: CartService;
-  let td: HTMLElement; // help me to get the value of the text content present in the 'td' element  
+  //let cartService: CartService;
+  //let td: HTMLElement; // help me to get the value of the text content present in the 'td' element  
+  //let demoCart: any;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ OrderComponent ],
-      providers: [CartService]
+      providers: [/*CartService*/]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(OrderComponent);
-    cartService = TestBed.get(CartService);
+    //cartService = TestBed.get(CartService);
     component = fixture.componentInstance;
-    fixture.detectChanges();
-    td = fixture.nativeElement.querySelector('td');
+    //fixture.detectChanges();
+    //td = fixture.nativeElement.querySelector('td');
+
+    
+
+    // code for jasmine.createSpyObj
+
+    /*demoCart = jasmine.createSpyObj('demoCart',
+    [
+      {
+      "id": 1,
+      "name": "test1",
+      "price": 100,
+      "quantity": 1,
+      },
+      {
+      "id": 2,
+      "name": "test2",
+      "price": 200,
+      "quantity": 2,
+      }
+    ]);
+    //demoCart.removeFromCart();
+    demoCart.getTotal();
+    */
   });
+  
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should assert value for "td" element to be value of componenet.product ',() => {
-    let p = component.getCart();
+  /*it('[spyOn] should chek getCart is called in order.component.html', () => {
+    let mockSpy = spyOn(cartService,'getTotal')
+    component.ngOnInit();
+    expect(mockSpy).toHaveBeenCalled();
+  });
+  */
+
+  /*it('should assert value for "td" element to be value of componenet.product ',() => {
+    const productMock = {
+      "name": "test",
+      "price": 100,
+      "quantity": 1
+    }
+    let cartSpy = spyOn(cartService, 'getSingle');
     fixture.detectChanges();
-    expect(td.textContent).toBe('test');
+    expect(td.textContent).toEqual('test');
+
+ 
+  });
+  */
 
 
+  it('should show list title "carrello"', () => {
+    expect(fixture.nativeElement.querySelector('[data-test="carrello"]')).toBeTruthy();
   });
 
+
+  it('should show list box area', () => {
+    expect(fixture.nativeElement.querySelector('[data-test="listBox"]')).toBeTruthy();
+  });
+
+
+  it('should show button for buy product', () => {
+    expect(fixture.nativeElement.querySelector('[data-test="buttonBuy"]')).toBeTruthy();
+  });
+
+
+  it('should show button for buy product', () => {
+    expect(fixture.nativeElement.querySelector('[data-test="buttonBuy"]')).toBeTruthy();
+  });
 
 });
