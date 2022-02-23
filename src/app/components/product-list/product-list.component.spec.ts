52c995989c761ba4cdaba8e8d80193d1fb6d666d
Mirko <mirko.verdecchia@studenti.unicam.it>
2022-02-23 18:32:04 +0100

    test for product-list UI element

--- src/app/components/product-list/product-list.component.spec.ts
+++ src/app/components/product-list/product-list.component.spec.ts
@@ -5,6 +5,7 @@ import { ProductListComponent } from './product-list.component';
 describe('ProductListComponent', () => {
   let component: ProductListComponent;
   let fixture: ComponentFixture<ProductListComponent>;
+  let demoCart: any;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -17,9 +18,48 @@ describe('ProductListComponent', () => {
     fixture = TestBed.createComponent(ProductListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    demoCart = jasmine.createSpyObj('demoCart',
+    [
+      {
+      "id": 1,
+      "name": "test1",
+      "price": 100,
+      "quantity": 1,
+      },
+      {
+      "id": 2,
+      "name": "test2",
+      "price": 200,
+      "quantity": 2,
+      }
+    ]);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
+  it('should show list title "listino"', () => {
+    expect(fixture.nativeElement.querySelector('[data-test="product-list"]')).toBeTruthy();
+  });
+
+
+  it('should show the listBox ', () => {
+    expect(fixture.nativeElement.querySelector('[id="ss_elem_list"]')).toBeTruthy();
+  });
+
+
+  it('should show the thead ', () => {
+    expect(fixture.nativeElement.querySelector('[data-test="productDetails"]')).toBeTruthy();
+  });
+
+/*
+  it('should show product details ', () => {
+    const product = fixture.nativeElement.querySelector('[data-test="product"]');
+    expect(product.querySelector('[data.test="title"]').innerText).toEqual('test1')
+  });
+*/
+  
 });
--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
 import { IProduct } from 'src/app/interface/product';
 import { CartService } from 'src/app/service/cart.service';
 
@@ -10,10 +11,29 @@ import { CartService } from 'src/app/service/cart.service';
 export class ProductListComponent implements OnInit {
 
   public products: IProduct[] = [];
+  //cart$: any;
 
   constructor(private cartService: CartService) { }
 
   ngOnInit(): void {
+    //this.cart$ = this.cartService.getSingle$();
+    /*this.cart$ = of([
+      {
+        name: "test1",
+        price: 10,
+        quantity: 1
+      },
+      {
+        name: "test2",
+        price: 20,
+        quantity: 2
+      },
+      {
+        name: "test3",
+        price: 30,
+        quantity: 3
+      }
+    ])*/
   }
 
   addProductToCart(Product: IProduct) {
