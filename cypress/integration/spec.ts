0ef05c15fef63f91323a1ca99b048c5d8981a29b
Mirko <mirko.verdecchia@studenti.unicam.it>
2022-03-11 10:16:09 +0100

    Failed Cypress test: -'shows product list' -'should add product from the list to the cart and show it' -'should remove product from the cart'

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -1,72 +1,92 @@
 describe('Nautes E-Commerce e2e Testing', () => {
-
   beforeEach(() => {
-      cy.visit('/');
-
+    cy.visit('/');
   });
 
   it('shows the LANDING page', () => {
-      cy.visit('/');
-      cy.contains('NEW E-COMMERCE!!');
+    cy.visit('/');
+    cy.contains('NEW E-COMMERCE!!');
   });
 
   it('shows the PRODUCT_LIST page', () => {
-
-      cy.get('.button2').click();
-      cy.get('body > app-root > app-product-list');
-
+    cy.get('.button2').click();
+    cy.get('body > app-root > app-product-list');
   });
-  
+
   it('shows the ORDER page', () => {
-      cy.get('.button3').click();
-      cy.get('body > app-root > app-order');
+    cy.get('.button3').click();
+    cy.get('body > app-root > app-order');
   });
 
   it('shows ALL the page', () => {
+    cy.get('.button2').click();
+    cy.get('body > app-root > app-product-list');
 
-      cy.get('.button2').click();
-      cy.get('body > app-root > app-product-list');
+    cy.get('.button3').click();
+    cy.get('body > app-root > app-order');
 
-      cy.get('.button3').click();
-      cy.get('body > app-root > app-order');
-
-      cy.get('.button1').click();
-      cy.get('body > app-root > app-home');
-
+    cy.get('.button1').click();
+    cy.get('body > app-root > app-home');
   });
-  
-  // it('shows product list', () => {
-  //     cy.get('.button2').click();
-  //     cy.get('body > app-root > app-product-list > div > div').should('have.length', '1');
-  // });
 
-  // it('should add product from the list to the cart and show it', () => {
+  it('shows product list', () => {
+    cy.get('.button2').click();
+    cy.get('.button1').click();
 
-  // cy.get('.button2').click();
-  // cy.get('.button').click();
-  // cy.get('.button3').click();
-  // cy.get('body > app-root > app-order > div > div').should('have.length', '1');
+    cy.get('.button2').click();
 
-  // });
+    cy.get('body > app-root > app-product-list > div > div').should(
+      'have.length',
+      '6'
+    );
+  });
+
+  it('should add product from the list to the cart and show it', () => {
+    cy.get('.button2').click();
+    cy.get('.button1').click();
+    cy.get('.button2').click();
+
+    cy.get('.button').click({ multiple: true });
+
+    cy.get('.button3').click();
+
+    cy.get('body > app-root > app-order > div > div').should(
+      'have.length',
+      '6'
+    );
+  });
+
+  it('should remove product from the cart', () => {
+    cy.get('.button2').click();
+    cy.get('.button1').click();
+    cy.get('.button2').click();
+
+    cy.get('.button').click({ multiple: true });
+
+    cy.get('.button3').click();
+
+    cy.get('body > app-root > app-order > div > div').should(
+      'have.length',
+      '6'
+    );
+
+    cy.get('.button3').click();
+    cy.get('.removeButton').click({ multiple: true });
+    cy.get('body > app-root > app-order > div > div').should(
+      'have.length',
+      '0'
+    );
+  });
 
   it('shows input text fields', () => {
+    cy.get('.button3').click();
 
-      cy.get('.button3').click();
-
-      cy.get('.phone');
-      cy.get('.city');
-      cy.get('.address');
-
+    cy.get('.phone');
+    cy.get('.city');
+    cy.get('.address');
   });
-
 });
 
 describe('Mobile App Testing', () => {
-
-
-  beforeEach(() => {
-      
-  });
-
-
+  beforeEach(() => {});
 });
