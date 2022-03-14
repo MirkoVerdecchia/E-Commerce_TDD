0b0aeb7bd95c542783fc180e28df427381bd13ba
Mirko <mirko.verdecchia@studenti.unicam.it>
2022-03-14 15:24:44 +0100

    Jasmine Test Failed: -should show logOut button link -should logOut button call logOut() -should show account button link

--- src/app/components/navbar/navbar.component.spec.ts
+++ src/app/components/navbar/navbar.component.spec.ts
@@ -38,4 +38,27 @@ describe('NavbarComponent', () => {
   it('should show login button link', () => {
     expect(fixture.nativeElement.querySelector('.button4')).toBeTruthy();
   });
+
+  it('should show logOut button link', () => {
+    expect(fixture.nativeElement.querySelector('.buttonLogout')).toBeTruthy();
+  });
+
+  it('should logOut button call logOut()', () => {
+    let spy = spyOn(component, 'logOut');
+
+    let button = fixture.debugElement.nativeElement.querySelector('.buttonLogout');
+    button.click();
+
+    fixture.whenStable().then(() => {
+      expect(spy).toHaveBeenCalled();
+    });
+  });
+  
+
+  it('should show account button link', () => {
+    expect(fixture.nativeElement.querySelector('.buttonAccount')).toBeTruthy();
+  });
+
+
+
 });
