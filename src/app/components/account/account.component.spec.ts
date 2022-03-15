a4c1f2c1cd63e92e903dcfad79ea8f9dcbeb0fd7
Mirko <mirko.verdecchia@studenti.unicam.it>
2022-03-15 11:31:42 +0100

    Jasmine Test Failed: -should show order

--- src/app/components/account/account.component.spec.ts
+++ src/app/components/account/account.component.spec.ts
@@ -2,13 +2,35 @@ import { HttpClient, HttpHandler } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { spyOnClass } from 'jasmine-es6-spies';
+import { IOrder } from 'src/app/interface/order';
 import { AccountService } from 'src/app/service/account.service';
-
+import { DataService } from 'src/app/service/data.service';
 import { AccountComponent } from './account.component';
 
 describe('AccountComponent', () => {
   let component: AccountComponent;
   let fixture: ComponentFixture<AccountComponent>;
+  let dataService: jasmine.SpyObj<DataService>;
+
+  const orders: IOrder[] = [
+    {
+      id: '1',
+      user: 'user@gmail.it',
+      phone: '3921899677',
+      city: 'Roma',
+      address: 'Via Dante Alighieri',
+      total: 1.5,
+      products: [
+        {
+          id: '1',
+          name: 'Pasta',
+          price: 1.5,
+          description: 'Pacco di pasta',
+        },
+      ],
+      date: '7/3/2022',
+    },
+  ];
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -18,18 +40,29 @@ describe('AccountComponent', () => {
           provide: AccountService,
           useFactory: () => spyOnClass(AccountService),
         },
+        {
+          provide: DataService,
+          useFactory: () => spyOnClass(DataService),
+        },
         HttpClient,
-        HttpHandler
+        HttpHandler,
       ],
       imports: [RouterTestingModule],
-
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AccountComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+  });
+
+  //beforeEach with the mock obj passed to the mock method
+  beforeEach(() => {
+    dataService = TestBed.get(DataService);
+
+    dataService.getOrdersByUser.and.returnValue(orders);
+
+    fixture.detectChanges(); //Detect the changes on the DOM in runtime
   });
 
   it('should create', () => {
@@ -44,13 +77,22 @@ describe('AccountComponent', () => {
     expect(fixture.nativeElement.querySelector('.orderTitle')).toBeTruthy();
   });
 
-  it('should show the order details', () => {
-    expect(fixture.nativeElement.querySelector('.id')).toBeTruthy();
-    expect(fixture.nativeElement.querySelector('.total')).toBeTruthy();
-    expect(fixture.nativeElement.querySelector('.date')).toBeTruthy();
-    expect(fixture.nativeElement.querySelector('.numberP')).toBeTruthy();
+  it('should show order', () => {
+    expect(
+      fixture.nativeElement.querySelectorAll('.orders').length
+    ).toBe(1);
   });
 
-
+  // it('should show the order details', () => {
+  //   const o = fixture.nativeElement.querySelectorAll('.orders');
+  //   for (var i = 0; i < o.length; i++) {
+  //     expect(o[i].querySelector('.id').innerText).toEqual(orders[i].id);
+  //     expect(o[i].querySelector('.total').innerText).toEqual(orders[i].total);
+  //     expect(o[i].querySelector('.date').innerText).toEqual(orders[i].date);
+  //     expect(o[i].querySelector('.numberP').innerText).toEqual(
+  //       orders[i].products
+  //     );
+  //   }
+  // });
 
 });
