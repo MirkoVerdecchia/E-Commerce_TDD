1a0849f8207c392dcdc6638b475afc06030eb808
Mirko <mirko.verdecchia@studenti.unicam.it>
2022-03-15 11:08:13 +0100

    create and delete product method

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -10,15 +10,16 @@ import { DataService } from 'src/app/service/data.service';
   styleUrls: ['./admin.component.css'],
 })
 export class AdminComponent implements OnInit {
-
   public products: IProduct[] = [];
 
-
-  constructor(private accountService: AccountService, private dataService: DataService, private router: Router) {}
+  constructor(
+    private accountService: AccountService,
+    private dataService: DataService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.products = this.dataService.getProduct();
-
 
     if (!this.accountService.isAdmin()) {
       this.router.navigateByUrl('account');
@@ -26,28 +27,33 @@ export class AdminComponent implements OnInit {
   }
 
   public deleteProduct(product: IProduct): void {
+    this.dataService.deleteProduct(product).subscribe(
+      () => console.log('Prodotto ' + product.id + ' eliminato'),
+      (error) => console.log(error)
+    );
   }
 
-  public createProduct(name: string, price: string, description: string): void {
+  public createProduct( name: string, price: string, description: string ): void {
+    var p: number =+ price;
+    if (this.checkDataProduct(name, p, description)) {
+      let product: IProduct = { id: '', name: name, price: p, description: description};
+      this.dataService.postProduct(product).subscribe(() => console.log('Prodotto inserito'), error => console.log(error))
+    }
   }
 
-  public checkDataProduct(name: string, price: number, description: string): boolean {
+  public checkDataProduct( name: string, price: number, description: string ): boolean {
     if (name.length <= 1) {
-      console.log("inserisci un numero corretto");
+      console.log('inserisci un numero corretto');
       return false;
     }
     if (price < 0) {
-      console.log("Inserisci un prezzo corretto");
+      console.log('Inserisci un prezzo corretto');
       return false;
     }
     if (description.length <= 10) {
-      console.log("Inserisci almeno 10 caratteri di descrizione");
+      console.log('Inserisci almeno 10 caratteri di descrizione');
       return false;
     }
     return true;
   }
-
-
-
-
 }
